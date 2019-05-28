6361e0a941001ccb5b29edc6a2596fe554118aa6
evy-goryunov <evy.goryunov@gmail.com>
2019-05-28 14:52:19 +0300

    first task done

---
+++ TypeOfDate/TypeOfDate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace TypeOfDate
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Type typeOfDateTime = typeof(DateTime);
+			foreach (var prop in typeOfDateTime.GetProperties()) { Console.WriteLine(prop.Name); }
+			Console.ReadKey();
+		}
+	}
+}
